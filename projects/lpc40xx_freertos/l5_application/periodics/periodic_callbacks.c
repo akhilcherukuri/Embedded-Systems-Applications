e38d05a3384057b151f17c9b524788e911189655
akhilcherukuri <akhilcherukuri@gmail.com>
2020-03-18 19:19:24 -0700

    CAN DBC LAB

--- projects/lpc40xx_freertos/l5_application/periodics/periodic_callbacks.c
+++ projects/lpc40xx_freertos/l5_application/periodics/periodic_callbacks.c
@@ -1,7 +1,9 @@
 #include "periodic_callbacks.h"
-
 #include "board_io.h"
 #include "gpio.h"
+
+#include "can_bus_initializer.h"
+#include "can_handler.h"
 
 /******************************************************************************
  * Your board will reset if the periodic function does not return within its deadline
@@ -10,28 +12,16 @@
  */
 void periodic_callbacks__initialize(void) {
   // This method is invoked once when the periodic tasks are created
+  init_can_driver();
 }
 
-void periodic_callbacks__1Hz(uint32_t callback_count) {
-  gpio__toggle(board_io__get_led0());
-  // Add your code here
-}
+void periodic_callbacks__1Hz(uint32_t callback_count) {}
 
 void periodic_callbacks__10Hz(uint32_t callback_count) {
-  gpio__toggle(board_io__get_led1());
-  // Add your code here
+  can_handler__transmit_messages_10hz();
+  can_handler__handle_all_incoming_messages();
+  can_handler__manage_mia_10hz();
 }
-void periodic_callbacks__100Hz(uint32_t callback_count) {
-  gpio__toggle(board_io__get_led2());
-  // Add your code here
-}
+void periodic_callbacks__100Hz(uint32_t callback_count) {}
 
-/**
- * @warning
- * This is a very fast 1ms task and care must be taken to use this
- * This may be disabled based on intialization of periodic_scheduler__initialize()
- */
-void periodic_callbacks__1000Hz(uint32_t callback_count) {
-  gpio__toggle(board_io__get_led3());
-  // Add your code here
-}
+void periodic_callbacks__1000Hz(uint32_t callback_count) {}
--- projects/lpc40xx_freertos/l5_application/periodics/test/test_periodic_callbacks.c
+++ projects/lpc40xx_freertos/l5_application/periodics/test/test_periodic_callbacks.c
@@ -7,6 +7,8 @@
 // - This will not pull the REAL source code of these modules (such as board_io.c)
 // - This will auto-generate "Mock" versions based on the header file
 #include "Mockboard_io.h"
+#include "Mockcan_bus_initializer.h"
+#include "Mockcan_handler.h"
 #include "Mockgpio.h"
 
 // Include the source we wish to test
@@ -16,11 +18,14 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-void test__periodic_callbacks__initialize(void) { periodic_callbacks__initialize(); }
+void test__periodic_callbacks__initialize(void) {
+  init_can_driver_Expect();
+  periodic_callbacks__initialize();
+}
 
-void test__periodic_callbacks__1Hz(void) {
-  gpio_s gpio = {};
-  board_io__get_led0_ExpectAndReturn(gpio);
-  gpio__toggle_Expect(gpio);
-  periodic_callbacks__1Hz(0);
+void test__periodic_callbacks__10Hz(void) {
+  can_handler__transmit_messages_10hz_Expect();
+  can_handler__handle_all_incoming_messages_Expect();
+  can_handler__manage_mia_10hz_Expect();
+  periodic_callbacks__10Hz(0);
 }
