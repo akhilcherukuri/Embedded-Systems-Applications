47641de17a08fa0d60505ba01b62739fa241aa36
akhilcherukuri <akhilcherukuri@gmail.com>
2020-03-18 19:17:05 -0700

    Can Mia Configurations

---
+++ projects/lpc40xx_freertos/l5_application/can_dbc_lab/can_mia_configurations.c
@@ -0,0 +1,7 @@
+#include "dbc_lab.h"
+
+const uint32_t dbc_mia_threshold_SENSOR_ACCELEROMETER = 200;
+const uint32_t dbc_mia_threshold_SENSOR_SWITCH = 200;
+
+const dbc_SENSOR_ACCELEROMETER_s dbc_mia_replacement_SENSOR_ACCELEROMETER;
+const dbc_SENSOR_SWITCH_s dbc_mia_replacement_SENSOR_SWITCH;
---
+++ projects/lpc40xx_freertos/l5_application/can_dbc_lab/test/test_can_bus_initializer.c
@@ -0,0 +1,18 @@
+#include "unity.h"
+
+#include <stdbool.h>
+
+#include "Mockcan_bus.h"
+
+#include "can_bus_initializer.c"
+
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void test_init_can_driver(void) {
+  can__init_ExpectAndReturn(can1, 100, 100, 100, NULL, NULL, true);
+  can__bypass_filter_accept_all_msgs_Expect();
+  can__reset_bus_Expect(can1);
+  init_can_driver();
+}
