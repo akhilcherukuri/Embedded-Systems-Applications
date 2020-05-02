80c7c1032b6487512b0d15efb56c985ee794f2cc
akhilcherukuri <akhilcherukuri@gmail.com>
2020-05-01 19:33:01 -0700

    problem2 edit

--- projects/lpc40xx_freertos/l5_application/midproblemtwo.c
+++ projects/lpc40xx_freertos/l5_application/midproblemtwo.c
@@ -1,16 +1,36 @@
 #include "midproblemtwo.h"
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 bool extract_string_between(const char *source, char *output_string, char between) {
-char line[];
-strcpy(line, source);
-  output_string = strtok(line, between);
-  output_string = strtok(NULL, between);
-// output_string = line.Remove(line.IndexOf(')')).Substring(source.IndexOf('(') + 1);
+  // char line[256];
+  // strcpy(line, source);
+  //   output_string = strtok(line, between);
+  //   output_string = strtok(NULL, between);
+  if (source == NULL) {
+    return false;
+  }
 
-
-
+  int len = strlen(source);
+  int startpt, endpt;
+  for (int i = 0; i < len; i++) {
+    if (source[i] == between) {
+      startpt = i;
+      break;
+    }
+  }
+  for (int i = i + 1; i < len; i++) {
+    if (source[i] == between) {
+      endpt = i;
+      break;
+    }
+  }
+  int final_length = startpt - endpt - 1;
+  for (int i = 0; i < final_length; i++) {
+    output_string[i] = source[startpt + i + 1];
+  }
+  return true;
 }
